9904168cbb99dbc81a2748a3ee6c37dc8fdf41e0
AdrianPousa-AlumnoCE <AdrianPousa-AlumnoCE@CEACFP10295>
2023-10-27 12:36:21 +0200

    clase 27/10 (2)

--- programacion/src/juegos/SegundoJuego_PPT.java
+++ programacion/src/juegos/SegundoJuego_PPT.java
@@ -24,92 +24,83 @@ public class SegundoJuego_PPT {
 		System.out.println("2. La IA");
 		System.out.println("");
 		
-		
 		int opcion = escogerOpcion();
-		String resultado ="";
+		juego(opcion);
 		
-		if(opcion == 1) {
-			String jugadaUsuario1 =pedirJugadaUsuario() ;
-			String jugadaUsuario2 =pedirJugadaUsuario() ;
-			resultado = juego(jugadaUsuario1, jugadaUsuario2);
-			
-		}else if (opcion == 2) {
-			String jugadaUsuario1 =pedirJugadaUsuario() ;
-			String jugadaIA =generarJugadaIA();
-			resultado = juego(jugadaUsuario1, jugadaIA);
+	}
+	
+	//Función que ejecuta el juego en función de la opcion escogida
+	public static void juego (int opcion){
+		String resultado = "Empate!";
+
+		if (opcion == 1) {
+			do {
+				String jugadaUsuario1 = pedirJugadaUsuario(1);
+				String jugadaUsuario2 = pedirJugadaUsuario(2);
+				resultado = resultado(jugadaUsuario1, jugadaUsuario2);
+				System.out.println();
+				System.out.println(resultado);
+			} while (resultado.equalsIgnoreCase("Empate!"));
 		}
-		
-		System.out.println();
-		System.out.println(resultado);
-		
+		if (opcion == 2) {
+			do {
+				String jugadaUsuario1 = pedirJugadaUsuario(1);
+				String jugadaIA = generarJugadaIA();
+				resultado = resultado(jugadaUsuario1, jugadaIA);
+				System.out.println();
+				System.out.println(resultado);
+			} while (resultado.equalsIgnoreCase("Empate!"));
+		}
 	}
 	
 	//Función para elegir el tipo de juego (vs Usuario / vs IA)
 	public static int escogerOpcion() {
 		Scanner sc = new Scanner (System.in);
 		int opcion = 0;
+		System.out.println("");
 		System.out.print("Pulse la opción que desea --> ");
 		try {
 			opcion = sc.nextInt();
 		} catch (InputMismatchException e) {
 			System.err.println( "No has escogido correctamente la opción " + e);
-			System.out.println("");
 			return escogerOpcion();
 		}
 		return opcion;
 	}
 	
-
-	
 	//Función en la que se compara la jugada del usuario y de la IA para comprobar el resultado (si empata se repite el juego)
-	public static String juego(String jugada1, String jugada2) {
-		
-		
+	public static String resultado(String jugada1, String jugada2) {
+				
 		if(jugada1.equalsIgnoreCase("piedra") && jugada2.equalsIgnoreCase("papel")) {
-			return "Jugada 1 = piedra  || Jugada 2 = papel  --> ¡GANA J2!";
-			
-			
+			return "Jugador 1 = piedra  || Jugador 2 = papel  --> ¡GANA JUGADOR 2!";						
 		}else if (jugada1.equalsIgnoreCase("papel") && jugada2.equalsIgnoreCase("piedra")) {
-			return "Jugada 1 = papel  || Jugada 2 = piedra  --> 'GANA J1!";
-			
-			
+			return "Jugador 1 = papel  || Jugador 2 = piedra  --> 'GANA JUGADOR 1!";						
 		}else if(jugada1.equalsIgnoreCase("piedra") && jugada2.equalsIgnoreCase("tijera")) {
-			return "Jugada 1 = piedra  || Jugada 2 = tijera  --> 'GANA J1!";
-			
-			
+			return "Jugador 1 = piedra  || Jugador 2 = tijera  --> 'GANA JUGADOR 1!";			
 		}else if (jugada1.equalsIgnoreCase("papel") && jugada2.equalsIgnoreCase("tijera")) {
-			return "Jugada 1 = tijera  || Jugada 2 = piedra  --> ¡GANA J2!";
-			
-			
+			return "Jugador 1 = tijera  || Jugador 2 = piedra  --> ¡GANA JUGADOR 2!";						
 		}else if(jugada1.equalsIgnoreCase("papel") && jugada2.equalsIgnoreCase("tijera")) {
-			return "Jugada 1 = papel  || Jugada 2 = tijera  --> ¡GANA J2!";
-			
-			
+			return "Jugador 1 = papel  || Jugador 2 = tijera  --> ¡GANA JUGADOR 2!";			
 		}else if (jugada1.equalsIgnoreCase("papel") && jugada2.equalsIgnoreCase("tijera")) {
-			return "Jugada 1 = tijera  || Jugada 2 = papel  --> 'GANA J1!";
-			
-			
+			return "Jugador 1 = tijera  || Jugador 2 = papel  --> 'GANA JUGADOR 1!";	
 		}
 		else {
 			System.out.println();
-			System.out.println("VAYA! HEMOS EMPATADO, VAMOS A REPETIR");
-			System.out.println();
-			return juego(pedirJugadaUsuario(), generarJugadaIA());
+			return "Empate!";
 		}
-		
 	}
 
 	//Función en la que se solicita al usuario que ingrese su jugada (piedra, papel o tijeras) 
 	//Da igual si es en mayúsculas o minúsculas
-	public static String pedirJugadaUsuario() {
+	public static String pedirJugadaUsuario(int jugador) {
 		System.out.println();
 		Scanner sc = new Scanner (System.in);
-		System.out.print("Introduce tú jugada: PIEDRA, PAPEL o TIJERA --> ");
+		System.out.print("Jugador " + jugador + " : PIEDRA, PAPEL o TIJERA --> ");
 		String jugadaUsuario = sc.nextLine();
 		if(!jugadaUsuario.equalsIgnoreCase("piedra") && !jugadaUsuario.equalsIgnoreCase("papel")  && !jugadaUsuario.equalsIgnoreCase("tijera")){
 			System.err.println("No te he entendido, introduce la palabra bien escrita");
 			System.out.println();
-			return pedirJugadaUsuario();
+			return pedirJugadaUsuario(jugador);
 		}
 		return jugadaUsuario;
 	}
@@ -120,7 +111,6 @@ public class SegundoJuego_PPT {
 		if(numAleatorio==-1) {
 			generarJugadaIA();
 		}
-		
 		switch(numAleatorio) {
 		case 0,1,2:
 			return "piedra";
@@ -129,5 +119,4 @@ public class SegundoJuego_PPT {
 		}
 		return "tijera";
 	}
-
 }
