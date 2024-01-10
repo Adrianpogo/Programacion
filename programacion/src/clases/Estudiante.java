86d699316711c8ec7ff82d382f80aa059201d87d
AdrianPousa-AlumnoCE <AdrianPousa-AlumnoCE@CEACFP10295>
2024-01-10 08:43:24 +0100

    fin clase 09/01 progra

---
+++ programacion/src/clases/Estudiante.java
@@ -0,0 +1,82 @@
+package clases;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class Estudiante {
+	
+	/* 
+	  Crear una clase llamada "Estudiante" que representa a un estudiante universitario.
+	  Funciones (métodos):
+	    Constructor: Un constructor que acepta el nombre y la edad del estudiante y lo inicializa.
+	    Método "agregarCurso": Un método que permite al estudiante agregar un curso a su lista de cursos inscritos.
+	    Método "agregarCalificacion": Un método que permite al estudiante agregar una calificación para un curso.
+	    Método "promedioCalificaciones": Un método que calcula y devuelve el promedio de calificaciones del estudiante.
+	  Atributos:
+	    Nombre del estudiante (String).
+	    Edad del estudiante (int).
+	    Una lista de cursos inscritos (List<String>).
+	    Un mapa que almacena las calificaciones de los cursos (Map<String, List<Double>>).
+	 */
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+    //ATRIBUTOS
+	public String nombre;
+	public int edad;
+	public List<String> cursosInscritos;
+	public Map<String, List<Double>> calificaciones;
+
+    //CONSTRUCTOR
+    public Estudiante(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.cursosInscritos = new ArrayList<>();
+        this.calificaciones = new HashMap<>();
+    }
+
+    //FUNCIONES
+    public void agregarCurso(String curso) {
+        cursosInscritos.add(curso);
+        calificaciones.put(curso, new ArrayList<>());
+    }
+
+    public void agregarCalificacion(String curso, double calificacion) {
+        if (calificaciones.containsKey(curso)) {
+            calificaciones.get(curso).add(calificacion);
+        } else {
+            System.out.println("El estudiante no está inscrito en el curso: " + curso);
+        }
+    }
+
+    public double promedioCalificaciones() {
+        double totalCalificaciones = 0;
+        int totalCursos = 0;
+
+        for (List<Double> calificacionesCurso : calificaciones.values()) {
+            for (Double calificacion : calificacionesCurso) {
+                totalCalificaciones += calificacion;
+                totalCursos++;
+            }
+        }
+
+        if (totalCursos > 0) {
+            return totalCalificaciones / totalCursos;
+        } else {
+            return 0.0;
+        }
+    }
+
+    //GET&SET
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+    
+}
---
+++ programacion/src/clases/Persona.java
@@ -0,0 +1,67 @@
+package clases;
+
+public class Persona {
+	
+	/* 
+	 
+	 Crear una clase llamada "Persona"
+		Funciones (métodos):
+			Constructor: Un constructor que acepte dos parámetros, el nombre (cadena de caracteres) y la edad (entero), y los utilice para inicializar los atributos de la clase.
+			Método "saludar": Un método llamado "saludar" que no tome ningún parámetro y muestre un mensaje de saludo en la consola, incluyendo el nombre de la persona.
+			Método "cumplirAnios": Un método llamado "cumplirAnios" que incremente la edad de la persona en 1 año cada vez que se llama.
+			Métodos getters y setters de todos los atributos
+		Atributos:	
+			Un atributo llamado "nombre" de tipo String para almacenar el nombre de la persona.
+			Un atributo llamado "edad" de tipo entero para almacenar la edad de la persona.
+		 	Un atributo llamado "sexo" de tipo booleano. 
+	  */
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	//ATRIBUTOS
+	public String nombre;
+	public int edad;
+	public boolean sexo;
+
+	//CONSTRUCTOR
+	public Persona(String nombre, int edad, boolean sexo) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.sexo=sexo;
+	}
+
+	//FUNCIONES
+	public void saludar() {
+		System.out.println("Hola, mi nombre es " + nombre);
+	}
+
+	public void cumplirAnios() {
+		edad++;
+	}
+
+	//GET&SET
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public boolean getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(boolean sexo) {
+		this.sexo = sexo;
+	}
+
+}
