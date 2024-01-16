c00dfb0a2e5e4678372d852b54a65cdd980ab7e3
AdrianPousa-AlumnoCE <AdrianPousa-AlumnoCE@CEACFP10295>
2024-01-16 11:43:01 +0100

    tarea_Tareas clase16/01

--- programacion/src/tareas_tarea/Main.java
+++ programacion/src/tareas_tarea/Main.java
@@ -4,6 +4,68 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		//Creamos las tareas
+		Tarea tarea1 = new Tarea("Limpiar la vajilla");
+		Tarea tarea2 = new Tarea("Fregar el suelo");
+		Tarea tarea3 = new Tarea("Limpiar los cristales");
+		Tarea tarea4 = new Tarea("Hacer la comida");
+		Tarea tarea5 = new Tarea("Descansar");
+		Tarea tarea6 = new Tarea("Hacer la cama");
+		Tarea tarea7 = new Tarea("Ducharse");
+		Tarea tarea8 = new Tarea("Lavarse los dientes");
+		Tarea tarea9 = new Tarea("Planchar la ropa");
+		
+		//Creamos el conjunto de tareas de la casa
+		Tareas tareasCasa = new Tareas();
+		
+		//Añadimos las tareas a la lista de las tareas
+		tareasCasa.agregarTarea(tarea1);
+		tareasCasa.agregarTarea(tarea2);
+		tareasCasa.agregarTarea(tarea3);
+		tareasCasa.agregarTarea(tarea4);
+		tareasCasa.agregarTarea(tarea5);
+		tareasCasa.agregarTarea(tarea6);
+		tareasCasa.agregarTarea(tarea7);
+		tareasCasa.agregarTarea(tarea8);
+		tareasCasa.agregarTarea(tarea9);
+		System.out.println("______________________________\n");
+		
+		//Mostramos todas las tareas
+		tareasCasa.mostrarTodasLasTareas();
+		System.out.println("______________________________\n");
+		
+		//Mostramos las tareas en x posiciones
+		System.out.println("Tarea1: " + tareasCasa.obtenerTarea(1).getDescripcion()); 
+		System.out.println("Tarea3: " +tareasCasa.obtenerTarea(3).getDescripcion()); 
+		System.out.println("Tarea6: " +tareasCasa.obtenerTarea(6).getDescripcion()); 
+		System.out.println("Tarea7: " +tareasCasa.obtenerTarea(7).getDescripcion()); 
+		System.out.println("______________________________\n");
+		
+		//Marcamos tareas como hechas
+		tareasCasa.marcarTareaComoCompletada(6);
+		tareasCasa.marcarTareaComoCompletada(7);
+		tareasCasa.marcarTareaComoCompletada(8);
+		
+		//Mostramos las tareas realizadas
+		tareasCasa.mostrarTareasCompletadas();
+		System.out.println("______________________________\n");
+		
+		//Mostramos las tareas pendientes
+		tareasCasa.mostrarTareasPendientes();
+		System.out.println("______________________________\n");
+		
+		//Mostramos el número de tareas pendientes
+		System.out.println("El número de tareas pendientes es: " + tareasCasa.contarTareasPendientes());
+		System.out.println("______________________________\n");
+		
+		//Eliminamos tareas realizadas
+		tareasCasa.eliminarTarea(6);
+		tareasCasa.eliminarTarea(7);
+		tareasCasa.eliminarTarea(8);
+		System.out.println("______________________________\n");
+		
+		//Mostramos de nuevo todas la tareas
+		tareasCasa.mostrarTodasLasTareas();
 		
 		
 	}
--- programacion/src/tareas_tarea/Tareas.java
+++ programacion/src/tareas_tarea/Tareas.java
@@ -5,95 +5,112 @@ import java.util.ArrayList;
 public class Tareas {
 
 	/*
-	 Propiedades de Clase:
-
-		listaDeTareas: Una lista que almacena las tareas pendientes.
-	Funciones:
-		agregarTarea(Tarea tarea): 
-			Agrega una tarea a la lista de tareas.
-
-		marcarTareaComoCompletada(int indice):
-		 	Marca una tarea como completada según su índice en la lista.
-
-		mostrarTareasPendientes():
-		 	Muestra todas las tareas pendientes en la lista.
-
-		mostrarTodasLasTareas():
-		 	Muestra todas las tareas, tanto pendientes como completadas.
-		
-		eliminarTarea(int indice):
-		 	Elimina una tarea específica de la lista de pendientes.
-		
-		mostrarTareasCompletadas(): 
-			Muestra todas las tareas que han sido marcadas como completadas.
-		
-		contarTareasPendientes(): 
-			Calcula el número total de tareas que aún están pendientes.
-		
-		obtenerTarea(int indice): 
-			Obtiene una tarea específica por su índice en la lista.
-
+	 * Propiedades de Clase:
+	 * 
+	 * listaDeTareas: Una lista que almacena las tareas pendientes. Funciones:
+	 * agregarTarea(Tarea tarea): Agrega una tarea a la lista de tareas.
+	 * 
+	 * marcarTareaComoCompletada(int indice): Marca una tarea como completada según
+	 * su índice en la lista.
+	 * 
+	 * mostrarTareasPendientes(): Muestra todas las tareas pendientes en la lista.
+	 * 
+	 * mostrarTodasLasTareas(): Muestra todas las tareas, tanto pendientes como
+	 * completadas.
+	 * 
+	 * eliminarTarea(int indice): Elimina una tarea específica de la lista de
+	 * pendientes.
+	 * 
+	 * mostrarTareasCompletadas(): Muestra todas las tareas que han sido marcadas
+	 * como completadas.
+	 * 
+	 * contarTareasPendientes(): Calcula el número total de tareas que aún están
+	 * pendientes.
+	 * 
+	 * obtenerTarea(int indice): Obtiene una tarea específica por su índice en la
+	 * lista.
+	 * 
 	 */
-	
+
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	
-	//ATRIBUTOS
+
+	// ATRIBUTOS
 	private ArrayList<Tarea> listaDeTareas;
-	
-	//CONSTRUCTOR
+
+	// CONSTRUCTOR
 	public Tareas() {
-        listaDeTareas = new ArrayList<>();
-    }
-	
-	//FUNCIONES
-	//Función que añade una tarea a la lista de tareas
+		listaDeTareas = new ArrayList<>();
+	}
+
+	// FUNCIONES
+	// Función que añade una tarea a la lista de tareas
 	public void agregarTarea(Tarea tarea) {
 		listaDeTareas.add(tarea);
 		System.out.println("Tarea añadida con éxito!");
 	}
-	
-	//Función para marcar una tarea como completada según su índice
+
+	// Función para marcar una tarea como completada según su índice
 	public void marcarTareaComoCompletada(int indice) {
 		if (indice >= 0 && indice < listaDeTareas.size()) {
 			Tarea tarea = listaDeTareas.get(indice);
-            tarea.marcarComoCompletada();
-		}else {
+			tarea.marcarComoCompletada();
+		} else {
 			System.out.println("El índice de la tarea no es válido.");
 		}
 	}
-	
-	//Función para mostrar todas las tareas pendientes
+
+	// Función para mostrar todas las tareas pendientes
 	public void mostrarTareasPendientes() {
 		System.out.println("Tareas sin completar: \n");
 		int contador = 1;
-		for(Tarea tarea : listaDeTareas) {
-			if(!tarea.estaCompletada()) {
+		for (Tarea tarea : listaDeTareas) {
+			if (!tarea.estaCompletada()) {
 				System.out.println(contador + ": " + tarea.getDescripcion());
-				contador ++;
+				contador++;
 			}
 		}
 	}
-	
-	//Función para mostrar todas las tareas 
+
+	// Función para mostrar todas las tareas
 	public void mostrarTodasLasTareas() {
-		
+		System.out.println("Tareas: \n");
+		int contador = 1;
+		for (Tarea tarea : listaDeTareas) {
+			System.out.println(contador + "- " + tarea.getDescripcion());
+			contador++;
+		}
 	}
-	
+
 	public void eliminarTarea(int indice) {
-		listaDeTareas.clear();
-		System.out.println("Se han eliminado todas las tareas");
+		if (indice <= listaDeTareas.size() && indice > 0) {
+			listaDeTareas.remove(indice);
+		}
+		System.out.println("Se ha eliminado la tarea");
 	}
-	
-	//Función para mostrar todas las tareas completadas
+
+	// Función para mostrar todas las tareas completadas
 	public void mostrarTareasCompletadas() {
-		
+		System.out.println("Tareas completadas: \n");
+		int contador = 1;
+		for (Tarea tarea : listaDeTareas) {
+			if (tarea.estaCompletada()) {
+				System.out.println(contador + ": " + tarea.getDescripcion());
+				contador++;
+			}
+		}
 	}
-	
-	//Función para contabilizar las tareas pendientes
-	public void contarTareasPendientes() {
-		
+
+	// Función para contabilizar las tareas pendientes
+	public int contarTareasPendientes() {
+		int contador = 0;
+		for (Tarea tarea : listaDeTareas) {
+			if (!tarea.estaCompletada()) {
+				contador++;
+			}
+		}
+		return contador;
 	}
-	
+
 	public Tarea obtenerTarea(int indice) {
 		if (indice >= 0 && indice < listaDeTareas.size()) {
 			return listaDeTareas.get(indice);
@@ -102,5 +119,5 @@ public class Tareas {
 			return null;
 		}
 	}
-	
+
 }
